900af0bc095c57c86ec4f39ff74ec5ba2fa14aed
airidosas252 <airidaslideikis@gmail.com>
2015-05-19 15:33:34 +0300

    Additional GPU freq steps

--- arch/arm/mach-msm/devices-msm7x27a.c
+++ arch/arm/mach-msm/devices-msm7x27a.c
@@ -896,20 +896,33 @@ static struct resource kgsl_3d0_resources[] = {
 
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
-		/* LGE_CHANGE_S [peter.jung@lge.com]
-		    DoU Power consumption */
+		{
+			.gpu_freq = 400000000,
+			.bus_freq = 200000000,
+		},
+		{
+			.gpu_freq = 350000000,
+			.bus_freq = 200000000,
+		},
 		{
 			.gpu_freq = 320000000,
 			.bus_freq = 200000000,
 		},
-		/* LGE_CHANGE_E [peter.jung@lge.com] */
+		{
+			.gpu_freq = 300000000,
+			.bus_freq = 200000000,
+		},
 		{
 			.gpu_freq = 245760000,
 			.bus_freq = 200000000,
 		},
 		{
 			.gpu_freq = 192000000,
-			.bus_freq = 160000000,
+			.bus_freq = 200000000,
+		},
+		{
+			.gpu_freq = 150000000,
+			.bus_freq = 200000000,
 		},
 		{
 			.gpu_freq = 133330000,
@@ -919,7 +932,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.init_level = 0,
 	/* LGE_CHANGE_S [peter.jung@lge.com]
 	     DoU Power consumption */
-	.num_levels = 4,
+	.num_levels = 8,
 	/* LGE_CHANGE_S [peter.jung@lge.com] */
 	.set_grp_async = set_grp_xbar_async,
 	.idle_timeout = HZ,
@@ -955,13 +968,27 @@ void __init msm8x25_kgsl_3d0_init(void)
 		kgsl_3d0_pdata.idle_timeout = HZ/5;
 		kgsl_3d0_pdata.strtstp_sleepwake = false;
 
-		if (SOCINFO_VERSION_MAJOR(socinfo_get_version()) >= 2)
-			/* 8x25 v2.0 & above supports a higher GPU frequency */
-			kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 320000000;
-		else
-			kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 300000000;
+			kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 400000000;
+			kgsl_3d0_pdata.pwrlevel[0].bus_freq = 200000000;
 
-		kgsl_3d0_pdata.pwrlevel[0].bus_freq = 200000000;
+			kgsl_3d0_pdata.pwrlevel[1].gpu_freq = 350000000;
+			kgsl_3d0_pdata.pwrlevel[1].bus_freq = 200000000;
+		
+			kgsl_3d0_pdata.pwrlevel[2].gpu_freq = 320000000;
+			kgsl_3d0_pdata.pwrlevel[2].bus_freq = 200000000;
+
+			kgsl_3d0_pdata.pwrlevel[3].gpu_freq = 300000000;
+			kgsl_3d0_pdata.pwrlevel[3].bus_freq = 200000000;
+
+			kgsl_3d0_pdata.pwrlevel[4].gpu_freq = 245760000;
+			kgsl_3d0_pdata.pwrlevel[4].bus_freq = 200000000;
+
+			kgsl_3d0_pdata.pwrlevel[5].gpu_freq = 192000000;
+			kgsl_3d0_pdata.pwrlevel[5].bus_freq = 200000000;
+
+			kgsl_3d0_pdata.pwrlevel[6].gpu_freq = 150000000;
+			kgsl_3d0_pdata.pwrlevel[6].bus_freq = 200000000;
+		
 	}
 }
 
